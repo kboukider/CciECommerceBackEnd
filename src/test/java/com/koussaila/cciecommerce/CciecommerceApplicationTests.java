cfc35f913820e069edf52b1259d5ae40f60fca6f
kboukider <kboukider@hotmail.com>
2020-12-02 17:37:15 +0100

    Projet CciECommerceBackEnd

---
+++ src/test/java/com/koussaila/cciecommerce/CciecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koussaila.cciecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CciecommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
